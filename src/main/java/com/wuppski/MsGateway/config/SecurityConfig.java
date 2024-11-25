406f29fd2c8d451e7a3d768a8736b5f440c575f1
jonathan rutter <jonathanrutter@gmail.com>
2024-11-25 21:27:19 +0000

    Add Swagger and SpringDoc to document RESTful endpoints Add a curcuit breaker using Resiliance4J

--- src/main/java/com/wuppski/MsGateway/config/SecurityConfig.java
+++ src/main/java/com/wuppski/MsGateway/config/SecurityConfig.java
@@ -9,10 +9,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] accessibleResourceURLs = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/api-docs/**",
+            "/aggregate/**"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth ->
-                auth.anyRequest().authenticated())
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(accessibleResourceURLs).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()))
                 .build();
